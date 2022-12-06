6a139210510e65ab011b6d4f0acd76a43f9aa771
fodedoumbouya <doumbouyaf.fode@gmail.com>
2022-12-06 10:11:34 +0100

    ajoute au script la copie des images

--- scripts/generateFile.sh
+++ scripts/generateFile.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 projectName=$1
-bootstrap=$2
-
 
 indexFile=$projectName/"index.html"
 indexCss=$projectName/css/"style.css"
+indexImage=$projectName/images/
+
 
 #----------------------------------------
   htmlTemplate="`cat ./template/html.txt`"
-  bootstrapHtmlTemplate="`cat ./template/bootstrapHtmlTemplate.txt`"
-  cssTemplate="`cat ./template/style.txt`"
+  # cssTemplate="`cat ./template/style.txt`"
 
 #-----------------------------------------
-  if [[ $bootstrap == 'b' ]]
-  then
-    echo $bootstrapHtmlTemplate > $indexFile
-  else
-    echo $htmlTemplate > $indexFile
-    echo $cssTemplate > $indexCss
-  fi
-  echo "Congratulations! Ton project est prete"
+#Fill up of index file from the template
+echo $htmlTemplate > $indexFile
+
+# Copie des images dans le dossier du project
+echo "`cp -r ./images/* "./$projectName/images/"`"
+
+
+# echo $cssTemplate > $indexCss
+#Message sucess
+echo "Congratulations! Ton project est prete"
