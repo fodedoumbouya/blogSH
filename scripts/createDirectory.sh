a5c807b56d109383a6d8e39a3e86dcc8f6302fda
Idricealy MOURTADHOI <idricealy@swello.com>
2022-12-06 09:19:12 +0100

    chore: delete dir script useless

--- scripts/createDirectory.sh
+++ scripts/createDirectory.sh
@@ -3,11 +3,9 @@ projectName=$1
 
   mkdir $projectName
   mkdir $projectName/css
-  mkdir $projectName/scripts
   mkdir $projectName/images
 
   touch $projectName/"index.html"
   touch $projectName/css/"style.css"
-  touch $projectName/scripts/"script.js"
 
   exec sh ./scripts/createProject.sh $projectName
