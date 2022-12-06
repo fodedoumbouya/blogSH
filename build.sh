c314f736cecf3bc3a3b0138c2f37829efb712ea8
fodedoumbouya <doumbouyaf.fode@gmail.com>
2022-12-06 09:30:48 +0100

    cleaning code

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 projectName=$1
-bootstrap=$2
 
 if [[ -d $projectName ]]
 then
   echo "This directory is exist already in your current directory."
 else
-
   # creation des Dossiers et les Fichiers
-  exec sh ./scripts/createDirectory.sh $projectName $bootstrap
+  exec sh ./scripts/createDirectory.sh $projectName
 fi
